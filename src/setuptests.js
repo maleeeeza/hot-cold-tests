b2d2bca645e4280a4d1804863405775f635e504a
maleeeeza <melissa.k.schoenberg@gmail.com>
2017-10-22 11:12:16 -0500

    more updates

--- src/setuptests.js
+++ src/setuptests.js
@@ -1,4 +1,8 @@
-import {configure} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-configure({adapter: new Adapter()});
+Enzyme.configure({ adapter: new Adapter() });
+
+global.requestAnimationFrame = function(callback) {
+  setTimeout(callback, 0);
+};
