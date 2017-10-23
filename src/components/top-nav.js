493a48f6a2caadd5bf55cd20c719553d76358d26
maleeeeza <melissa.k.schoenberg@gmail.com>
2017-10-22 19:22:56 -0500

    updated yml and hrefs

--- src/components/top-nav.js
+++ src/components/top-nav.js
@@ -22,12 +22,12 @@ export default class TopNav extends React.Component {
             <nav>
                 <ul className="clearfix">
                     <li>
-                        <a className="what" href="#" onClick={e => this.onInfo(e)}>
+                        <a className="what" href="#what" onClick={e => this.onInfo(e)}>
                             What?
                         </a>
                     </li>
                     <li>
-                        <a className="new" href="#" onClick={e => this.onNewGame(e)}>
+                        <a className="new" href="#new" onClick={e => this.onNewGame(e)}>
                             + New Game
                         </a>
                     </li>
@@ -36,4 +36,3 @@ export default class TopNav extends React.Component {
         );
     }
 };
-
